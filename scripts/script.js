314a4b192f9533a469cc09af6480228dbf0df653
Arthur Vershyhora <ArturiTunes@gmail.com>
2018-10-12 07:25:58 +0300

    Add isSpace function etc code

--- scripts/script.js
+++ scripts/script.js
@@ -1,10 +1,11 @@
 let sortButton = document.querySelector('.sortButton');
 let resetButton = document.querySelector('.resetButton');
+let userWords = [];
 
 sortButton.addEventListener('click', function (e) {
 	e.preventDefault();
 	let userText = document.querySelector('textarea');
-	console.log(userText.value);
+	setArrayWords(userText.value);
 }, false);
 resetButton.addEventListener('click', function (e) {
 	e.preventDefault();
@@ -12,3 +13,34 @@ resetButton.addEventListener('click', function (e) {
 	userText.value = '';
 }, false);
 
+function isSpace(text){
+	if (text.length > 0) {
+		let space = text.indexOf(' ');
+		if (space === -1) return false;
+		else if (space !== -1) return space;
+	};
+};
+
+function setArrayWords(userText){
+	if (userText.length > 0) {
+		let space = userText.indexOf(' ');
+		let word = "";
+		if (isSpace !== -1) {
+			word = userText.slice(userText[0], isSpace);
+			userWords.push(word);
+
+		} else if (isSpace === -1) {
+			word = userText;
+		};
+		
+		if (true) {}	
+	};
+};
+
+function sort(){
+	let newUserText = userText.slice(isSpace+1);
+			if (newUserText.length > 0) {
+				console.log(`New user text is: ${newUserText}`);
+				setArrayWords(newUserText);
+			};
+};
